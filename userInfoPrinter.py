1c05213d59b2ff79a7c1a1e56e2724f9d4d22e89
adhan06 <hanson.alex@yahoo.com>
2015-08-19 23:34:08 -0500

    Added first simple program
    Asks for name, age, and reddit username printing back the results.

---
+++ userInfoPrinter.py
@@ -0,0 +1,5 @@
+name = raw_input("What is your name? ")
+age = raw_input("What is your age? ")
+username = raw_input("What is your reddit username? ")
+
+print "Your name is " + name + ", you are " + str(age) + " years old, and your username is " + username
